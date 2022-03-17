15b6991f384e8fc66d41621513551e092f01b0f1
Ivan <novostroievv@mail.ru>
2022-03-17 23:59:51 +0300

    дописал функции

--- FinishedClassTasks/Task2/Task2/main.cpp
+++ FinishedClassTasks/Task2/Task2/main.cpp
@@ -6,78 +6,105 @@
 #include <string>
 using namespace std;
 
-/* Задание: дописать пропущенные методы, выделить функции, классы,
-заменить часть функций перегрузкой операторов.
-
-ЦЕЛЬ: main() должен стать хорошо читаемой структрутой, чтобы было понятно, что происходит во всей программе (без комментариев),
-но при этом помещался бы на одном экране.
-
-! В main() вносить свои изменения структуры программы запрещено. Только заменять действия на аналоги, реализованные в
-функциях или с использованием классов. СТРУКТУРА ДОЛЖНА ОСТАТЬСЯ ИСХОДНОЙ.
-
-На гитхабе должно появиться 4 коммита:
-0. дописал методы
-1. выделил функции
-2. выделил классы и применил принципы ООП
-3. заменил часть функций операторами
-
-То есть по окончании каждого этапа делайте коммит.
-
-Подсказки:
-0. Возможные классы, которые проглядыываются в программе:
-- класс МЕНЮ (объединяет функции, относящиеся к меню взаимодействия с пользователем),
-- класс ТОЧКА (для хранения координат)
-- класс ТРЕУГОЛЬНИК (в конструкторах должны отображаться все способы задания треугольника).
-
-1. Функции:
-- различные функции меню (показать меню 1 уровня, показать меню 2 уровня и т.д.),
-- функции ввода и проверки корректности данных,
-- функции различных действий над треугольником: проверка на существование, подсчёт площади и т.д.
-
-Указание. На этапе создания классов предполагается, что объект класса создаётся не в данной функции, а после неё.
-Не лепите ввод данных и конструктор в один метод. Функции ввода данных - функции МЕНЮ, а создание объекта (точки, например) -
-функции соответствующего класса.
-
-Пример допустимой реализации:
-Через 3 метода:
-1) метод ввода значений (метод класса меню) - inputSides
-2) метод ввода треугльника (метод класса треугольник) -inputTriangle
-
-Далее используется перечисление - enum Type {bySides, byBaseAndHeight, byPointsCoords};
-
-Triange tr1;
-tr1.inputTriangle(bySides);
-
-void inputTriangle(Type _type) {
-  switch (_type) {
-   case bySides:
-     int side1, side2, side3;
-     inputSides(&side1, &side2, &side3);   // тут сидит ввод значений
-     Triange temp(side1, side2, side3);    // тут создаётся треугольник с указанными полями
-     this = temp;
-     break;
-   // ...
-  }
-}
-
-или
-
-void inputTriangle(Type _type) {
-  switch (_type) {
-   case bySides:
-     int side1, side2, side3;
-     inputSides(&side1, &side2, &side3);   // тут сидит ввод значений
-     this->side1 = side1;                  // тут изменяется уже существующий треугольник, заполнение указанными полями
-     this->side2 = side2;
-     this->side3 = side3;
-     break;
-   // ...
-  }
-}
-
-*/
-
 enum Type { bySides, byBaseAndHeight, byPointsCoords };
+
+void typeOftriangle (int AB, int BC, int AC) {
+    if (AB == AC && AC == BC)
+        cout << "Type of triangle: equilateral" << endl;
+    else if ((AB == AC && AC != BC) || (AB == BC && BC != AC) || (AC == BC && BC != AB))
+        cout << "Type of triangle: isosceles" << endl;
+    else if ((AB == sqrt(pow(AC, 2) + pow(BC, 2))) || (BC == sqrt(pow(AC, 2) + pow(AB, 2))) || (AC == sqrt(pow(AB, 2) + pow(BC, 2))))
+        cout << "Type of triangle: right triangle" << endl;
+    else cout << "Type of triangle: scalene" << endl;
+}
+void squareComp (float S1) {
+    float S2;
+    cout << "Enter the square of another triangle: ";
+    cin >> S2;
+    if (S1 > S2)
+        cout << "Your square " << S1 << " > " << S2 << endl;
+    else if (S1 < S2)
+        cout << "Your square " << S1 << " < " << S2 << endl;
+    else cout << "Your square " << S1 << " = " << S2 << endl;
+}
+float squareSide (int AB, int BC, int AC) {
+    int p = (AB + BC + AC) * 0.5;
+    return sqrt(p * (p - AB) * (p - BC) * (p - AC));
+}
+float squareH (int AB, int H) {
+    return AB * H * 0.5;
+}
+float squarePoints (int *A, int *B, int *C) {
+    return 0.5 * abs((B[0] - A[0]) * (C[1] - A[1]) - (C[0] - A[0]) * (B[1] - A[1]));
+}
+void checkS_Sides (int AB, int BC, int AC, int userChoosen) {
+    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
+        int S = squareSide(AB, BC, AC);
+        if (userChoosen == 1) {
+            cout << "Square of triangle: S = " << S << endl;
+        }
+        else if (userChoosen == 2) {
+            int P = AB + BC + AC;
+            cout << "Square of triangle: P = " << P << endl;
+        }
+        else if (userChoosen == 3) {
+            typeOftriangle(AB, BC, AC);
+        }
+        else if (userChoosen == 4){
+            squareComp(S);
+        }
+    }
+    else {
+        cout << "Input error: triangle dont exist" << endl;
+    }
+}
+void checkS_H(int AB, int H, int userChoosen) {
+    if (AB > 0 && H > 0) {
+        int S = squareH(AB, H);
+        if (userChoosen == 1) {
+            cout << "Square of triangle: S = " << S << endl;
+        }
+        else if (userChoosen == 2 || userChoosen == 3) {
+            cout << "Input error: we couldn't find type of triangles by your parameters" << endl;
+        }
+        else if (userChoosen == 4){
+            squareComp(S);
+        }
+    }
+    else {
+        cout << "Input error: triangle dont exist" << endl;
+    }
+}
+void checkS_Points(int* A, int* B, int* C, int userChoosen) {
+    int AB = sqrt(pow(B[0] - A[0], 2) + pow(B[1] - A[1], 2));
+    int BC = sqrt(pow(C[0] - B[0], 2) + pow(C[1] - B[1], 2));
+    int AC = sqrt(pow(C[0] - A[0], 2) + pow(C[1] - A[1], 2));
+    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
+        if (userChoosen == 1) {
+            int S = squarePoints(A, B, C);
+            cout << "Square of triangle: S = " << S << endl;
+        }
+        else if (userChoosen == 2) {
+            int P = AB + BC + AC;
+            cout << "Square of triangle: P = " << P << endl;
+        }
+        else if (userChoosen == 3) {
+            typeOftriangle(AB, BC, AC);
+        }
+        else if (userChoosen == 4) {
+            int S = squarePoints(A, B, C);
+            squareComp(S);
+        }
+    }
+    else {
+        cout << "Input error: triangle dont exist" << endl;
+    }
+}
+
+void toFirstMenu() {
+    system("pause");
+    system("cls");
+}
 
 int main() {
     int user_choosen;
@@ -148,140 +175,51 @@ int main() {
                 break;
             case 1:
                 if (_inputType == bySides) {
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        p = (AB + BC + AC) * 0.5;
-                        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
-                        cout << "Square of triangle: S = " << S << endl;
-                    }
-                    else {
-                        cout << "Input error: triangle dont exist" << endl;
-                    }
+                    checkS_Sides(AB, BC, AC, user_choosen);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    if (AB > 0 && h > 0) {
-                        S = AB * h * 0.5;
-                        cout << "Square of triangle: S = " << S << endl;
-                    }
-                    else {
-                        cout << "Input error: triangle dont exist" << endl;
-                    }
+                    checkS_H(AB, h, user_choosen);
                 }
                 else if (_inputType == byPointsCoords) {
-                    AB = sqrt(pow(B[0] - A[0], 2) + pow(B[1] - A[1], 2));
-                    BC = sqrt(pow(C[0] - B[0], 2) + pow(C[1] - B[1], 2));
-                    AC = sqrt(pow(C[0] - A[0], 2) + pow(C[1] - A[1], 2));
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        p = (AB + BC + AC) * 0.5;
-                        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
-                        cout << "Square of triangle: S = " << S << endl;
-                    }
-                    else {
-                        cout << "Input error: triangle dont exist" << endl;
-                    }
+                    checkS_Points(A, B, C, user_choosen);
                 }
-                system("pause");
-                system("cls");
+                toFirstMenu();
                 break;
             case 2:
                 if (_inputType == bySides) {
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        P = AB + BC + AC;
-                        cout << "Square of triangle: P = " << P << endl;
-                    }
-                    else {
-                        cout << "Input error: triangle dont exist" << endl;
-                    }
+                    checkS_Sides(AB, BC, AC, user_choosen);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    cout << "Input error: we couldn't find perimeter by your parameters" << endl;
+                    checkS_H(AB, h, user_choosen);
                 }
                 else if (_inputType == byPointsCoords) {
-                    AB = sqrt(pow(B[0] - A[0], 2) + pow(B[1] - A[1], 2));
-                    BC = sqrt(pow(C[0] - B[0], 2) + pow(C[1] - B[1], 2));
-                    AC = sqrt(pow(C[0] - A[0], 2) + pow(C[1] - A[1], 2));
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        P = AB + BC + AC;
-                        cout << "Square of triangle: P = " << P << endl;
-                    }
-                    else {
-                        cout << "Input error: triangle dont exist" << endl;
-                    }
+                    checkS_Points(A, B, C, user_choosen);
                 }
+                toFirstMenu();
                 break;
             case 3:
                 if (_inputType == bySides) {
-                    if (AB == AC && AC == BC)
-                        cout << "Type of triangle: equilateral" << endl;
-                    else if ((AB == AC && AC != BC) || (AB == BC && BC != AC) || (AC == BC && BC != AB))
-                        cout << "Type of triangle: isosceles" << endl;
-                    else if ((AB == sqrt(pow(AC, 2) + pow(BC, 2))) || (BC == sqrt(pow(AC, 2) + pow(AB, 2))) || (AC == sqrt(pow(AB, 2) + pow(BC, 2))))
-                        cout << "Type of triangle: right triangle" << endl;
-                    else cout << "Type of triangle: scalene" << endl;
+                    checkS_Sides(AB, BC, AC, user_choosen);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    cout << "Input error: we couldn't find type of triangles by your parameters" << endl;
+                    checkS_H(AB, h, user_choosen);
                 }
                 else if (_inputType == byPointsCoords) {
-                    AB = sqrt(pow(B[0] - A[0], 2) + pow(B[1] - A[1], 2));
-                    BC = sqrt(pow(C[0] - B[0], 2) + pow(C[1] - B[1], 2));
-                    AC = sqrt(pow(C[0] - A[0], 2) + pow(C[1] - A[1], 2));
-                    if (AB == AC && AC == BC)
-                        cout << "Type of triangle: equilateral" << endl;
-                    else if ((AB == AC && AC != BC) || (AB == BC && BC != AC) || (AC == BC && BC != AB))
-                        cout << "Type of triangle: isosceles" << endl;
-                    else if ((AB == sqrt(pow(AC, 2) + pow(BC, 2))) || (BC == sqrt(pow(AC, 2) + pow(AB, 2))) || (AC == sqrt(pow(AB, 2) + pow(BC, 2))))
-                        cout << "Type of triangle: right triangle" << endl;
-                    else cout << "Type of triangle: scalene" << endl;
+                    checkS_Points(A, B, C, user_choosen);
                 }
+                toFirstMenu();
                 break;
             case 4:
-                float S2;
-                cout << "Enter the square of another triangle: ";
-                cin >> S2;
                 if (_inputType == bySides) {
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        p = (AB + BC + AC) * 0.5;
-                        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
-                        if (S > S2)
-                            cout << "Your square " << S << " > " << S2 << endl;
-                        else if (S < S2)
-                            cout << "Your square " << S << " < " << S2 << endl;
-                        else cout << "Your square " << S << " = " << S2 << endl;
-                    }
-                    else {
-                        cout << "Input error: Your triangle dont exist" << endl;
-                    }
+                    checkS_Sides(AB, BC, AC, user_choosen);
                 }
                 else if (_inputType == byBaseAndHeight) {
-                    if (AB > 0 && h > 0) {
-                        S = AB * h * 0.5;
-                        if (S > S2)
-                            cout << "Your square " << S << " > " << S2 << endl;
-                        else if (S < S2)
-                            cout << "Your square " << S << " < " << S2 << endl;
-                        else cout << "Your square " << S << " = " << S2 << endl;
-                    }
-                    else {
-                        cout << "Input error: Your triangle dont exist" << endl;
-                    }
+                    checkS_H(AB, h, user_choosen);
                 }
                 else if (_inputType == byPointsCoords) {
-                    AB = sqrt(pow(B[0] - A[0], 2) + pow(B[1] - A[1], 2));
-                    BC = sqrt(pow(C[0] - B[0], 2) + pow(C[1] - B[1], 2));
-                    AC = sqrt(pow(C[0] - A[0], 2) + pow(C[1] - A[1], 2));
-                    if (AB + BC > AC && AB + AC > BC && AC + BC > AB) {
-                        p = (AB + BC + AC) * 0.5;
-                        S = sqrt(p * (p - AB) * (p - BC) * (p - AC));
-                        if (S > S2)
-                            cout << "Your square " << S << " > " << S2 << endl;
-                        else if (S < S2)
-                            cout << "Your square " << S << " < " << S2 << endl;
-                        else cout << "Your square " << S << " = " << S2 << endl;
-                    }
-                    else {
-                        cout << "Input error: Your triangle dont exist" << endl;
-                    }
+                    checkS_Points(A, B, C, user_choosen);
                 }
+                toFirstMenu();
                 break;
             case 5:
                 back_flag = true;
@@ -289,7 +227,6 @@ int main() {
             default:
                 cout << "Input error: error in input" << endl;
             }
-
             if (back_flag || exit_flag) {
                 system("cls");
                 cout << "Last parameters: ";
