b16300ab95dfe34ce324c66aa11144e0c6040c53
Марина Усова <oppabang@outlook.com>
2022-03-31 07:36:33 +0300

    add some project and deleted extra files

--- AllPrograms/ExampleWithCodingStyle/Class.h
+++ AllPrograms/ExampleWithCodingStyle/Class.h
@@ -1,1 +1,10 @@
 #pragma once
+#include <iostream>
+
+class Point {
+  int x;
+  int y;
+public:
+  Point() { x = 0; y= 0; };
+  void print() { std::cout << "(" << x << ", " << y << ")"; }
+};
---
+++ AllPrograms/ExampleWithCodingStyle/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "Class.h"
+
+int main() {
+  Point x;
+  x.print();
+
+  int a,b;
+  a = 5;
+  b =7;
+  int sum = a+b;
+  system("pause");
+};
