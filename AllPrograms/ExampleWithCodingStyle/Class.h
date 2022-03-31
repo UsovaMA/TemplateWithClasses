33e6673f0b05d5b32660c55019a83297529f72b3
Марина Усова <oppabang@outlook.com>
2022-03-31 07:48:47 +0300

    fix extra ; in class (coding style)

--- AllPrograms/ExampleWithCodingStyle/Class.h
+++ AllPrograms/ExampleWithCodingStyle/Class.h
@@ -6,6 +6,6 @@ class Point {
   int x;
   int y;
  public:
-  Point() { x = 0; y = 0; };
-  void print() { std::cout << "(" << x << ", " << y << ")"; };
+  Point() { x = 0; y = 0; }
+  void print() { std::cout << "(" << x << ", " << y << ")"; }
 };
